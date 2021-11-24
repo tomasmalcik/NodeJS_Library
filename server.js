45aec9c8fe2e3fdb5ca9e784a3298bc58ef32ff3
Tomas_Malcik <tomas.malcik@tul.cz>
2021-11-24 17:13:46 +0100

    Initial Author browse

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-if (process.env.NODE_ENV != "production") {
-    require("dotenv").config()
-}
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
 
 const express = require("express")
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
 const indexRouter = require("./routes/index")
+const authorsRouter = require("./routes/authors")
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
 
 app.set("view engine", "ejs")
 app.set("views", __dirname + "/views")
 app.set("layout", __dirname + "/views/layouts/layout")
 app.use(expressLayouts)
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 /* MONGOOSE */
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
@@ -25,5 +28,8 @@ db.once("open", () => { console.log("connected to mongoose") })
 
 /*  ROUTES  */
 app.use("/", indexRouter)
+app.use("/authors", authorsRouter)
+
+app
 
 app.listen(process.env.PORT || 3000)
